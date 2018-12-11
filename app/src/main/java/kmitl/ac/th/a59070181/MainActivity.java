85b67bec47116861a73dc3796f1332228176c33f
SurasithIT <59070181@it.kmitl.ac.th>
2018-12-11 10:03:44 +0700

    First commit

---
+++ app/src/main/java/kmitl/ac/th/a59070181/MainActivity.java
@@ -0,0 +1,13 @@
+package kmitl.ac.th.a59070181;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
